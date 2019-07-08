dc15cc6ce3399f432fe3c061072cde7a4aba5eac
spuken <bear110739@gmail.com>
2019-07-08 22:04:08 +0800

    add show

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -23,6 +23,11 @@ class Admin::RestaurantsController < ApplicationController
         end
     end
     
+    def show
+        @restaurant = Restaurant.find(params[:id])
+    end
+    
+
     private
 
     def restaurant_params
