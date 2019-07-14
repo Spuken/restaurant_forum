affc3bbae4a35a8d4169f7d3df0320cdf123d3ac
spuken <bear110739@gmail.com>
2019-07-14 16:48:22 +0800

    add feed and dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,29 @@
 Rails.application.routes.draw do
   
+  #user
   get 'users/:id' => 'users#show' , as: 'sp_user'
   get 'users/:id/edit' => 'users#edit',  as: 'sp_edit_users'
   put 'users/:id' => 'users#update'
   patch 'users/:id' => 'users#update'
   
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "restaurants#index"
+
   resources :restaurants, only: [:index, :show] do
+    # restaurants/:id/comments/
     resources :comments, only: [:create, :destroy]
+
+    # restaurants/feeds
+    collection do
+      get :feeds
+    end
+
+    # restaurants/:id/feeds
+    member do
+      get :dashboard
+    end
   end
 
   resources :categories, only: :show
