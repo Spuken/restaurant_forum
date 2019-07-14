055b3a8bc1c070cd70fa2bb64ed74766ce904267
spuken <bear110739@gmail.com>
2019-07-14 17:48:51 +0800

    add feeds and model connection

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
     validates_presence_of :name, on: :create, message: "can't be blank m"
     
-    has_many :restaurants, dependent: :destroy
+    has_many :restaurants, dependent: :restrict_with_error
 end
